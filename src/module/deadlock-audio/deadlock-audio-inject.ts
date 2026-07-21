7f1009c214b2c6faaaed8e3f82d3ffd76452ce8f
Bernardo Lopes <bernardo.lopes@outlook.com>
2026-07-21 17:19:27 -0300

    Fix hangups when checking other Deadlock strings.

--- src/module/deadlock-audio/deadlock-audio-inject.ts
+++ src/module/deadlock-audio/deadlock-audio-inject.ts
@@ -2,24 +2,46 @@ import {observeElementEvenIfNotReady, postExtensionMessage} from "../../util/uti
 import {requestSettings} from "../../common/extension-settings-client";
 import {ExtensionMessageId} from "../../common/extension-message";
 
-observeElementEvenIfNotReady(".string-key-container--text", (e) => {
-    const wrapperId = "dlaudio-play-wrapper";
-    let playWrapper = document.querySelector(`#${wrapperId}`);
-    if (playWrapper === undefined || playWrapper === null) {
-       playWrapper = document.createElement("div");
-       playWrapper.id = wrapperId;
-        const wrapper = document.querySelector(".string-key-container--wrapper");
-        wrapper.after(playWrapper);
-    }
-    playWrapper.replaceChildren();
-    // @ts-expect-error crowdin not available while programming
-    if (window.crowdin.translation.file_id === 17229) {
-        const element = playElement(e.innerText);
-        playWrapper.replaceChildren(element);
-    }
-}, true);
+const WRAPPER_ID = "dlaudio_play_wrapper";
 
-function playElement(fileName: string): HTMLMediaElement {
+
+function hook(immediate: boolean) {
+    observeElementEvenIfNotReady("#translation_text_container", (element, disconnect) => {
+        let playWrapper = document.querySelector(`#${WRAPPER_ID}`);
+        // @ts-expect-error crowdin not available while programming
+        if (window.crowdin.translation.file_id !== 17229) {
+            disconnect();
+            playWrapper?.remove();
+            hook(false);
+            return;
+        }
+        const e = element.querySelector(".string-key-container--text")
+        if (e === null || !(e instanceof HTMLElement)) {
+            return;
+        }
+        if (playWrapper === null || !(playWrapper instanceof HTMLElement)) {
+            playWrapper = document.createElement("div");
+            playWrapper.id = WRAPPER_ID;
+            const wrapper = document.querySelector(".string-key-container--wrapper");
+            playWrapper?.remove();
+            wrapper.after(playWrapper);
+        }
+        const fileName = e.innerText;
+        if ((playWrapper as HTMLElement).dataset.currentFile === fileName) {
+            return;
+        }
+        disconnect();
+        playWrapper.replaceChildren();
+        const playElement = createPlayElement(e.innerText);
+        (playWrapper as HTMLElement).dataset.currentFile  = fileName;
+        playWrapper.replaceChildren(playElement);
+        hook(false);
+    }, immediate);
+}
+
+hook(true);
+
+function createPlayElement(fileName: string): HTMLMediaElement {
     const element = document.createElement("audio");
     element.src = `http://localhost:51072/audio/${fileName}`;
     element.controls = true;
